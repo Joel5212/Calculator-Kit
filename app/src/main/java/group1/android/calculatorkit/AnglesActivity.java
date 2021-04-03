d6e6ab57270b473ad039d71bb0e8019172f3aa54
lchua2314 <lchua2314@gmail.com>
2021-04-03 12:31:46 -0700

    Added main menu and geometry calculator menu

---
+++ app/src/main/java/group1/android/calculatorkit/AnglesActivity.java
@@ -0,0 +1,13 @@
+package group1.android.calculatorkit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class AnglesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angles);
+    }
+}

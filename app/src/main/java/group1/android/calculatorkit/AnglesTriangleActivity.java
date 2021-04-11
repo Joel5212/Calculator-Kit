136a4a64c5c044b5ac5a6cb570c67b68b97c7514
lchua2314 <lchua2314@gmail.com>
2021-04-10 21:06:18 -0700

    Savedinstancestate 2

--- app/src/main/java/group1/android/calculatorkit/AnglesTriangleActivity.java
+++ app/src/main/java/group1/android/calculatorkit/AnglesTriangleActivity.java
@@ -12,6 +12,9 @@ import android.widget.Toast;
 
 public class AnglesTriangleActivity extends AppCompatActivity {
 
+    // Used for savedInstanceState
+    private static final String KEY_INDEX = "index";
+
     EditText etAngle1;
     EditText etAngle2;
     Button btnFindMissingAngle;
@@ -29,6 +32,15 @@ public class AnglesTriangleActivity extends AppCompatActivity {
 
         etAngle1.setTransformationMethod(new NumericKeyBoardTransformationMethod());
         etAngle2.setTransformationMethod(new NumericKeyBoardTransformationMethod());
+
+        // Checks if savedInstanceState is null
+        // This happens because it hasn't been initialized yet
+        // until after first activity is created
+        // If not null, retrieve the missing angle data
+        // This makes sure the data gets retrieved after configuration changes.
+        if (savedInstanceState != null) {
+            tvMissingAngle.setText(savedInstanceState.getString(KEY_INDEX, "Please Input Angle(s)"));
+        }
 
         btnFindMissingAngle.setOnClickListener(v -> {
 
@@ -139,6 +151,15 @@ public class AnglesTriangleActivity extends AppCompatActivity {
         return true;
     }
 
+
+    /*
+    Saves missing angle answer across configuration changes.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(KEY_INDEX, tvMissingAngle.getText().toString());
+    }
 
     /**
      * Class is needed to change password input text that shows bullets as text
